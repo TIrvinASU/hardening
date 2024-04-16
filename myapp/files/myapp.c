95ce9852b8b3d28fd7266b28d3fbd96fa244f90a
TIrvinASU <thirvin@asu.edu>
2024-04-16 06:54:39 -0700

    commiting new device tree and regression tests

--- myapp/files/myapp.c
+++ myapp/files/myapp.c
@@ -27,33 +27,117 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 
-#define INIT_STATE_PATH "/sys/module/mali/initstate"
-#define MAX_STATE_LENGTH 20
+#define MAX_BUF_SIZE 1024
+
+int checkdir(const char *path){
+	DIR *dir = opendir(path);
+	if (dir != NULL) {
+		closedir(dir);
+		return 1;
+	}
+	return 0;
+}
+
+void nodecheck(){
+	printf("Checking nodes by path...\n");
+	const char *gpupath = "/sys/devices/platform/axi/fd4b0000.gpu";
+	const char *qspipath = "/sys/devices/platform/axi/ff0f0000.spi";
+	const char *usbone = "/sys/devices/platform/axi/ff9e0000.usb1";
+	const char *display = "/sys/devices/platform/axi/fd4a0000.display";
+	const char *cci = "/sys/devices/platform/axi/fd6e0000.cci";
+	const char *smmu = "/sys/devices/platform/axi/fd800000.smmu";
+	if (checkdir(gpupath)) {
+		printf("GPU node is up.\n");
+	} else {
+		printf("GPU node is down!\n");
+	}
+	if (checkdir(qspipath)) {
+		printf("QSPI node is up.\n");
+	} else {
+		printf("QSPI node is down!\n");
+	}
+	if (checkdir(usbone)) {
+		printf("usb1 is up.\n");
+	} else {
+		printf("usb1 is down!\n");
+	}
+	if (checkdir(display)) {
+		printf("display port is up.\n");
+	} else {
+		printf("display port is down!\n");
+	}
+	if (checkdir(cci)) {
+		printf("cci is up.\n");
+	} else {
+		printf("cci is down!\n");
+	}
+	if (checkdir(smmu)) {
+		printf("smmu is up.\n");
+	} else {
+		printf("smmu is down!\n");
+	}
+}
+
+int parsedmesg(){
+	FILE *pipe;
+	char buffer[MAX_BUF_SIZE];
+	int qspistate = 0;
+	int usbonestate = 0;
+	int usbonesub = 0;
+	int displayp = 0;
+	int cci = 0;
+	printf("Parsing DMESG...\n");
+	pipe = popen("dmesg", "r");
+	if (pipe == NULL) {
+		perror("popen");
+		return 0;
+	}
+	while (fgets(buffer, MAX_BUF_SIZE, pipe) != NULL) {
+		if (strstr(buffer, "ff0f0000.spi: deferred probe timeout") != NULL) {
+			printf("QSPI probed!\n");
+			qspistate = 1;
+		}
+		if (strstr(buffer, "probe of ff9e0000.usb1") != NULL) {
+			printf("Attempted probe of usb1!\n");
+			usbonestate = 1;
+		}
+		if (strstr(buffer, "dwc3 fe200000.usb: deferred probe timeout") != NULL) {
+                        printf("Attempted probe of usb1 child!\n");
+                        usbonesub = 1;
+                }
+		if (strstr(buffer, "ZynqMP DisplayPort Subsystem driver probed") != NULL) {
+			printf("DisplayPort Probed!\n");
+			displayp = 1;
+		}
+		if (strstr(buffer, "ARM CCI_400_r1 PMU driver probed") != NULL) {
+			printf("cci probed!\n");
+			cci = 1;
+		}
+	}
+	pclose(pipe);
+	if (qspistate == 0) {
+		printf("qspi down!\n");
+	}
+	if (usbonestate == 0) {
+		printf("usb1 down!\n");
+	}
+	if (usbonesub == 0) {
+		printf("usb1 child node down!\n");
+	}
+	if (displayp == 0) {
+		printf("display port down!\n");
+	}
+	if (cci == 0) {
+		printf("cci is down!\n");
+	}
+	return 1;
+}
 
 int main(int argc, char **argv) {
-    FILE *initstate_file;
-    char initstate[MAX_STATE_LENGTH];
-
-    // Open the initstate file for reading
-    initstate_file = fopen(INIT_STATE_PATH, "r");
-    if (initstate_file == NULL) {
-        perror("Error opening initstate file");
-        return 1;
-    }
-
-    // Read the initialization state from the file
-    if (fgets(initstate, MAX_STATE_LENGTH, initstate_file) == NULL) {
-        perror("Error reading initstate file");
-        fclose(initstate_file);
-        return 1;
-    }
-
-    // Close the file
-    fclose(initstate_file);
-
-    // Print the initialization state
-    printf("Mali GPU Initialization State: %s", initstate);
-
-    return 0;
+	nodecheck();
+	parsedmesg();
+	return 0;
 }
