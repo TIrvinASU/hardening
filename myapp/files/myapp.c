e7eb8dee6fab494c766eee61c1ad8e0954ace182
TIrvinASU <thirvin@asu.edu>
2024-04-17 00:22:18 -0700

    More tests added, properly deleted usb

--- myapp/files/myapp.c
+++ myapp/files/myapp.c
@@ -32,6 +32,7 @@
 
 #define MAX_BUF_SIZE 1024
 
+
 int checkdir(const char *path){
 	DIR *dir = opendir(path);
 	if (dir != NULL) {
@@ -46,9 +47,14 @@ void nodecheck(){
 	const char *gpupath = "/sys/devices/platform/axi/fd4b0000.gpu";
 	const char *qspipath = "/sys/devices/platform/axi/ff0f0000.spi";
 	const char *usbone = "/sys/devices/platform/axi/ff9e0000.usb1";
+	const char *usbsub = "/sys/devices/platform/axi/ff9e0000.usb1/of_node/usb@fe300000";
 	const char *display = "/sys/devices/platform/axi/fd4a0000.display";
 	const char *cci = "/sys/devices/platform/axi/fd6e0000.cci";
 	const char *smmu = "/sys/devices/platform/axi/fd800000.smmu";
+	const char *perfa = "/sys/devices/platform/axi/ffa00000.perf-monitor";
+	const char *perfb = "/sys/devices/platform/axi/fd0b0000.perf-monitor";
+	const char *perfc = "/sys/devices/platform/axi/fd490000.perf-monitor";
+	const char *perfd = "/sys/devices/platform/axi/ffa10000.perf-monitor";
 	if (checkdir(gpupath)) {
 		printf("GPU node is up.\n");
 	} else {
@@ -63,6 +69,11 @@ void nodecheck(){
 		printf("usb1 is up.\n");
 	} else {
 		printf("usb1 is down!\n");
+	}
+	if (checkdir(usbsub)) {
+		printf("usb1 child is up.\n");
+	} else {
+		printf("usb1 child is down!\n");
 	}
 	if (checkdir(display)) {
 		printf("display port is up.\n");
@@ -79,6 +90,43 @@ void nodecheck(){
 	} else {
 		printf("smmu is down!\n");
 	}
+	if (checkdir(perfa)) {
+		printf("perf-monitor ocm is up.\n");
+	} else {
+		printf("perf-monitor ocm is down.\n");
+	}
+	if (checkdir(perfb)) {
+                printf("perf-monitor ddr is up.\n");
+        } else {
+                printf("perf-monitor ddr is down.\n");
+        }
+	if (checkdir(perfc)) {
+                printf("perf-monitor cci is up.\n");
+        } else {
+                printf("perf-monitor cci is down.\n");
+        }
+	if (checkdir(perfd)) {
+                printf("perf-monitor lpd is up.\n");
+        } else {
+                printf("perf-monitor lpd is down.\n");
+        }
+	printf("\n");
+}
+
+void usbstatus(){
+	FILE *fp;
+	const char *filepath = "/sys/devices/platform/axi/ff9d0000.usb0/power/runtime_status";
+	char file_content[1024];
+	fp = fopen(filepath, "r");
+	printf("Checking usb0 status...\n");
+	if (fp == NULL) {
+		perror("Error opening file");
+	}
+	while (fgets(file_content, sizeof(file_content), fp)) {
+		printf("%s", file_content);
+	}
+	fclose(fp);
+	printf("\n");
 }
 
 int parsedmesg(){
@@ -86,9 +134,13 @@ int parsedmesg(){
 	char buffer[MAX_BUF_SIZE];
 	int qspistate = 0;
 	int usbonestate = 0;
-	int usbonesub = 0;
+	int usbzerosub = 0;
 	int displayp = 0;
 	int cci = 0;
+	int perfa = 0;
+	int perfb = 0;
+	int perfc = 0;
+	int perfd = 0;
 	printf("Parsing DMESG...\n");
 	pipe = popen("dmesg", "r");
 	if (pipe == NULL) {
@@ -104,9 +156,9 @@ int parsedmesg(){
 			printf("Attempted probe of usb1!\n");
 			usbonestate = 1;
 		}
-		if (strstr(buffer, "dwc3 fe200000.usb: deferred probe timeout") != NULL) {
-                        printf("Attempted probe of usb1 child!\n");
-                        usbonesub = 1;
+		if (strstr(buffer, "dwc3 fe300000.usb: deferred probe timeout") != NULL) {
+                        printf("usb0 child probed!\n");
+                        usbzerosub = 1;
                 }
 		if (strstr(buffer, "ZynqMP DisplayPort Subsystem driver probed") != NULL) {
 			printf("DisplayPort Probed!\n");
@@ -116,6 +168,22 @@ int parsedmesg(){
 			printf("cci probed!\n");
 			cci = 1;
 		}
+		if (strstr(buffer, "ffa00000.perf-monitor: Probed") != NULL) {
+			printf("perf-monitor ocm probed!\n");
+			perfa = 1;
+		}
+		if (strstr(buffer, "fd0b0000.perf-monitor: Probed") != NULL) {
+                        printf("perf-monitor ddr probed!\n");
+                        perfb = 1;
+                }
+		if (strstr(buffer, "fd490000.perf-monitor: Probed") != NULL) {
+                        printf("perf-monitor cci probed!\n");
+                        perfc = 1;
+                }
+		if (strstr(buffer, "ffa10000.perf-monitor: Probed") != NULL) {
+                        printf("perf-monitor lpd probed!\n");
+                        perfd = 1;
+                }
 	}
 	pclose(pipe);
 	if (qspistate == 0) {
@@ -124,7 +192,7 @@ int parsedmesg(){
 	if (usbonestate == 0) {
 		printf("usb1 down!\n");
 	}
-	if (usbonesub == 0) {
+	if (usbzerosub == 0) {
 		printf("usb1 child node down!\n");
 	}
 	if (displayp == 0) {
@@ -133,11 +201,25 @@ int parsedmesg(){
 	if (cci == 0) {
 		printf("cci is down!\n");
 	}
+	if (perfa == 0) {
+		printf("perf-monitor ocm is down!\n");
+	}
+	if (perfb == 0) {
+                printf("perf-monitor ddr is down!\n");
+        }
+	if (perfc == 0) {
+                printf("perf-monitor cci is down!\n");
+        }
+	if (perfd == 0) {
+                printf("perf-monitor lpd is down!\n");
+        }
+	printf("\n");
 	return 1;
 }
 
 int main(int argc, char **argv) {
 	nodecheck();
 	parsedmesg();
+	usbstatus();
 	return 0;
 }
